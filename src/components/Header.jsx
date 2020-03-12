700bc8fc6a2c6daf3b85be87addb0ec454822cb0
Andrey <Xenysoid@gmail.com>
2020-03-12 20:17:29 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return(
+  <header className="header">
+    <img src="https://www.indirtik.com/wp-content/uploads/2019/07/logo-maker-transparent-background-4.png" />
+  </header>
+  )
+};
+
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+  return(
+    <div className="content">
+    <div>
+      
+      <img
+        className="ava"
+        src="https://www.nastol.com.ua/pic/201302/1024x1024/nastol.com.ua-40414.jpg"
+      ></img>
+      Кот Василий
+  
+    </div>
+    <div>
+      <img src="https://avatars.mds.yandex.net/get-pdb/907231/09738fd8-6068-4d90-902f-c3c2733bfa6d/s1200?webp=false"></img>
+    </div>
+    <div>
+      My posts
+      <div>New post</div>
+    </div>
+  </div>
+  )
+};
+
+
+export default Profile
