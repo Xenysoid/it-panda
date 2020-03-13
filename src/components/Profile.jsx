357f41414765412178a0824e29dd0b810688ae98
Andrey <Xenysoid@gmail.com>
2020-03-13 15:48:19 +0300

    add module-css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import s from './Profile.module.css'
 
 const Profile = () => {
   return(
-    <div className="content">
+    <div className={s.content}>
     <div>
       
       <img
-        className="ava"
+        className={s.ava}
         src="https://www.nastol.com.ua/pic/201302/1024x1024/nastol.com.ua-40414.jpg"
       ></img>
       Кот Василий
@@ -15,9 +16,11 @@ const Profile = () => {
     <div>
       <img src="https://avatars.mds.yandex.net/get-pdb/907231/09738fd8-6068-4d90-902f-c3c2733bfa6d/s1200?webp=false"></img>
     </div>
-    <div>
+    <div className={s.posts}>
       My posts
-      <div>New post</div>
+      <div className={s.item}>New post1</div>
+      <div className={s.item}>New post2</div>
+      <div className={s.item}>New post3</div>
     </div>
   </div>
   )
