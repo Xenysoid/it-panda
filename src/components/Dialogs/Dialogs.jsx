9c8800a0effb310bd921689c893e1d9dc63bb1f0
Andrey <Xenysoid@gmail.com>
2020-03-14 21:42:21 +0300

    Work in Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs__items}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    John
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className="message">Hi</div>
+                <div className="message">How are you?</div>
+                <div className="message">Yo</div>
+                <div className="message">I'm fun</div>
+                <div className="message">Goodbye</div>
+                <div className="message">Bye</div>
+            </div>
         </div>
     )
 }
